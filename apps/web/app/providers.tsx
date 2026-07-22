d0a57e0590bd2c1bb7c140415ae8a21b3dd69878
sushilbishowkarma-rumsan <sushil.bishowkarma@rumsan.net>
2026-07-22 16:20:21 +0545

    TEST THE USER

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -37,6 +37,14 @@ function createWagmiConfig() {
   })
 }
 
+//jckahf ciajcjaskcsjcliajkc doduvei ckjh  9jlfjjcn iho ccmhb ic 
+// ;cpou cj'smnc8 hc
+// kcj 9pj
+// C\
+// 'OJ CCPJC L'OM OCJ
+// PCC 
+// 'I CKJ D[  
+// ;PC; CUHK 0'
 
 function AuthedRainbowKit({ children }: { children: ReactNode }) {
   const reactQueryClient = useQueryClient()
