fe5399dafdab0ca8431d75c3c8424190cc1ea6de
me-Sushil <me.sushilbishowkarma@gmail.com>
2026-07-27 21:04:57 +0545

    swaggger added

---
+++ apps/api/dist/common/swagger/swagger.constants.d.ts
@@ -0,0 +1,10 @@
+export declare const SWAGGER_PATH = "docs";
+export declare const SESSION_COOKIE_AUTH = "session-cookie";
+export declare const API_TAGS: {
+    readonly HEALTH: "Health";
+    readonly AUTH: "Authentication";
+    readonly ISSUER: "Issuer";
+    readonly ADMIN: "Admin";
+    readonly DOCUMENTS: "Documents";
+    readonly PDF: "PDF";
+};
